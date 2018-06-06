9001ee246ce3a8cb5a5687d0789179d23a276d4c
aCodingTurtle <34428554+aCodingTurtle@users.noreply.github.com>
2018-06-06 13:46:08 -0400

    Update MiniKings.py

--- MiniKings.py
+++ MiniKings.py
@@ -1,2 +1,21 @@
 #MINIKINGS v0.0.1
 
+def scout(space):
+  #stuff
+  
+def war(space):
+  #stuff - different from fighting
+
+def dHappenings():
+  #stuff
+  
+dIssues = {
+  'item one' = 1,
+  'stuff' = 0
+}
+
+def fHappenings():
+  #stuff
+ 
+def manageGov():
+  #stuff
